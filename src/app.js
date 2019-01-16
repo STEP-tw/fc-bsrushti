1a6ab1a971d84592ec536407d4c83c3a3885a51e
Srushti <bsrushti@in-bsrushti.local>
2019-01-16 14:10:51 +0530

    Linked index.html with server.js
    * Previously it can be accessible with python server
    * Created separate folder for js files of html pages

--- src/app.js
+++ src/app.js
@@ -1,5 +1,22 @@
+const fs = require("fs");
+
 const app = (req, res) => {
+  if (req.url == "/") {
+    req.url = "/index.html";
+  }
+  fs.readFile("./src" + req.url, function(err, contents) {
+    if (err) {
+      return;
+    }
+    res.write(contents);
+    res.end();
+  });
+  sendNotFound(req, res);
+};
+
+const sendNotFound = (req, res) => {
   res.statusCode = 404;
+  res.write("Not Found");
   res.end();
 };
 
