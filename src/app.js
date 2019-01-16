273009f51a77740e4a1f2a386ebed8e36f7754d8
Srushti <bsrushti@in-bsrushti.local>
2019-01-16 10:59:59 +0530

    Introduces UI for flower catalog

--- src/app.js
+++ src/app.js
@@ -3,6 +3,22 @@ const app = (req, res) => {
   res.end();
 };
 
+const logRequest = (req, res, next) => {
+  console.log(req.method, req.url);
+  console.log("headers =>", JSON.stringify(req.headers, null, 2));
+  console.log("body =>", req.body);
+  next();
+};
+
+const readBody = (req, res, next) => {
+  let content = "";
+  req.on("data", chunk => (content += chunk));
+  req.on("end", () => {
+    req.body = content;
+    next();
+  });
+};
+
 // Export a function that can act as a handler
 
 module.exports = app;
