77e3103e12e7aebc6b89544d8f7dc14096a34f94
Srushti <bsrushti@in-bsrushti.local>
2019-01-18 09:27:46 +0530

    Introduces guestBook.html as per second requirement
    * Introduces framework to handle post request
    * Writing data to the log file, parsing is remain

--- src/app.js
+++ src/app.js
@@ -1,23 +1,55 @@
 const fs = require("fs");
+const App = require("./frameWork");
+const app = new App();
 
-const app = (req, res) => {
-  if (req.url == "/") req.url = "/index.html";
-  fs.readFile("./public" + req.url, function(err, contents) {
+const readFile = (req, res) => {
+  let fileName = req.url;
+  if (fileName == "/") fileName = "/index.html";
+  fs.readFile("./public" + fileName, function(err, contents) {
     if (err) {
-      sendNotFound(req, res);
+      sendResponse(res, "NOT FOUND", 404);
       return;
     }
-    res.write(contents);
-    res.end();
+    sendResponse(res, contents, 200);
   });
 };
 
-const sendNotFound = (req, res) => {
-  res.statusCode = 404;
-  res.write("Not Found");
+const sendResponse = function(res, content, status) {
+  res.statusCode = status;
+  res.write(content);
   res.end();
 };
 
+const readBody = (req, res, next) => {
+  let content = "";
+  req.on("data", chunk => (content += chunk));
+  req.on("end", () => {
+    req.body = content;
+    console.log("readBody:", req.body);
+    next();
+  });
+};
+
+const logRequest = (req, res, next) => {
+  console.log(req.method, req.url);
+  console.log("headers =>", JSON.stringify(req.headers, null, 2));
+  console.log("body =>", req.body);
+  next();
+};
+
+const home = (req, res) => {
+  readFile(req, res);
+};
+
+const guestBook = (req, res) => {
+  sendResponse(res, req.body, 200);
+};
+
+app.use(readBody);
+app.use(logRequest);
+app.post("/log", guestBook);
+app.use(home);
+
 // Export a function that can act as a handler
 
-module.exports = app;
+module.exports = app.handleRequest.bind(app);
